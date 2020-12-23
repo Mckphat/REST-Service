8364b7b0ba4ec6900f12bf681113a7e97f05906d
Kannika McKenzie Phatsavong <44480342+Mckphat@users.noreply.github.com>
2020-12-22 23:03:54 -0500

    Add files via upload

---
+++ Payroll/src/main/java/com/tts/Payroll/Status.java
@@ -0,0 +1,8 @@
+package com.tts.Payroll;
+
+public enum Status {
+	IN_PROGRESS, //
+	 COMPLETED, //
+	 CANCELLED
+
+}
---
+++ Payroll/src/test/java/com/tts/Payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.Payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
